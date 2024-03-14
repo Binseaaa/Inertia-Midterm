390e725118331d234f8381b72b7b108ed3839635
Vincy Deguma <vinsmar8888@gmail.com>
2024-03-14 11:16:49 +0800

    third commit

---
+++ resources/js/Pages/Creditors/Create.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderBar from '@/Components/HeaderBar.vue';
+import {Head} from '@inertiajs/vue3'
+</script>
+
+<template>
+    <Head title="Create Creditor"></Head>
+    <HeaderBar/>
+
+
+</template>
